bc2bad3ad1d7a91711f236e4ff4ba6b500a364d0
Piyush <kmrpiyush95@gmail.com>
2025-09-02 18:38:00 +0530

    smart-notepa-ui

---
+++ article-hub-frontend/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './shared/material-module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxUiLoaderConfig, NgxUiLoaderModule, PB_DIRECTION, SPINNER } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig : NgxUiLoaderConfig ={
+  text:"Loading...",
+  textColor:"white",
+  textPosition:"center-center",
+  bgsColor:"white",
+  fgsColor:"white",
+  fgsType: SPINNER.ballSpinClockwise,
+  fgsSize:100,
+  pbDirection:PB_DIRECTION.leftToRight,
+  pbThickness: 5
+
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
